17d9ae3272e93e60df151c209b8645bcf9e75b96
EngFaisalWaleed <eng.faisalwaleed@gmail.com>
2022-04-12 23:30:02 +0500

    Configured deep linking for development and standalone expo app

--- src/main/Navigation.js
+++ src/main/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as Linking from "expo-linking";
 
 import * as navigators from "../constants/navigators";
 import About from "../screens/About/About";
@@ -9,9 +10,15 @@ import Home from "../screens/Home/Home";
 
 const Tab = createBottomTabNavigator();
 
+const prefix = Linking.createURL("/");
+
 const Navigation = () => {
+  const linking = {
+    prefixes: [prefix],
+  };
+
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <Tab.Navigator>
         <Tab.Screen name={navigators.HOME_SCREEN} component={Home} />
         <Tab.Screen name={navigators.ABOUT_SCREEN} component={About} />
